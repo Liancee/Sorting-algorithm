8151046f0f69e6b781d65c418d00027bb65fc4ff
Linden <franz.linden@astendo.de>
2020-06-15 11:44:01 +0200

    Selection_sort
    first commit

---
+++ Sorting-algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Selection_Sort.selectSort(new[] { 7, 4, 12, 1, 2 }).ToList().ForEach(x => Console.WriteLine(x));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sorting-algorithm/Selection-Sort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_algorithm
+{
+    class Selection_Sort
+    {
+        public static int[] selectSort(int[] arr)
+        {
+            int min, temp;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (arr[j] < arr[min])
+                        min = j;
+                if (min != i)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+            return arr;
+        }
+    }
+}
